6476a05fbdedac28725c6df63f263dca80752d59
Acker <acker2880@gmail.com>
2023-08-01 01:03:26 +0800

    fixed problemsets create validation

--- Aio-f/src/app/problem-sets/create/create.component.ts
+++ Aio-f/src/app/problem-sets/create/create.component.ts
@@ -1,43 +1,73 @@
 import { Component } from '@angular/core';
-import { NavigationStart, Router } from '@angular/router';
+import { Router } from '@angular/router';
 import { ProblemSetsService } from '../problem-sets.service';
 import {
-	FormBuilder,
-	FormGroup,
-	FormArray,
-	Validators,
-	AbstractControl,
-	FormControl
+  FormBuilder,
+  FormGroup,
 } from '@angular/forms';
+import { environment } from 'src/environments/environment';
+import { ValidatorService } from '../../helpers';
+import { AlertService, XStatus } from '../../shared';
 
 @Component({
-	selector: 'app-problem-sets-create',
-	templateUrl: './create.component.html',
-	styleUrls: ['./create.component.scss']
+  selector: 'app-problem-sets-create',
+  templateUrl: './create.component.html',
+  styleUrls: ['./create.component.scss'],
 })
 export class CreateComponent {
-	form: FormGroup;
-	title: AbstractControl;
-	description: AbstractControl;
+  form: FormGroup;
 
-	constructor(private router: Router,
-              private fb: FormBuilder,
-              private problemSetsService: ProblemSetsService) {
-	}
+  status: XStatus = XStatus.Ready;
+  XStatus = XStatus;
+  envs = environment;
 
-	ngOnInit(): void {
-		this.form = this.fb.group({
-			title: ['', Validators.required],
-			description: ['', Validators.required],
-		});
+  constructor(
+    private router: Router,
+    private fb: FormBuilder,
+    private problemSetsService: ProblemSetsService,
+    private alertService: AlertService,
+    private validator: ValidatorService,
+  ) {}
 
-		this.title = this.form.controls['title'];
-		this.description = this.form.controls['description'];
-	}
+  ngOnInit(): void {
+    this.form = this.fb.group({
+      title: [
+        '',
+        this.validator.checkTitle.bind(this.validator, 'problemSets.title'),
+      ],
+      description: [
+        '',
+        this.validator.checkContent.bind(
+          this.validator,
+          'problemSets.description'
+        ),
+      ],
+    });
 
-	onSubmit(form: any) {
-		console.log(this.problemSetsService.create(form));
-	}
+  }
+
+  get f() {
+    return this.form.controls;
+  }
+
+  onSubmit(form: any) {
+    this.status = XStatus.Clicked;
+
+    // Stop here if form is invalid
+    if (this.form.invalid) {
+      return;
+    }
+
+    this.problemSetsService.create(form).subscribe({
+      next: (data) => {
+        this.status = XStatus.Succeed;
+        this.alertService.success('alerts.createSucceed');
+        this.router.navigate(['/problem-sets', data.id]);
+      },
+      error: (err) => {
+        this.status = XStatus.Failed;
+        this.alertService.error(err);
+      }
+    });
+  }
 }
-
-
